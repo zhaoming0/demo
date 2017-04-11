2bd44b95ed2555f24a9498a4c8aaedb6f9b788a6
zhaoming0 <mingx.zhao@intel.com>
2017-04-11 13:12:49 +0800

    pudate

--- resources/testharness.js
+++ resources/testharness.js
@@ -932,9 +932,11 @@ policies and contribution forms [3].
         assert(!same_value(actual, expected), "assert_not_equals", description,
                                               "got disallowed value ${actual}",
                                               {actual:actual});
+        console.log("DEBUG-77");
         console.log(actual);
         console.log("DEBUG-8");
         console.log(expected);
+        console.log("DEBUG-99");
     }
     expose(assert_not_equals, "assert_not_equals");
 
