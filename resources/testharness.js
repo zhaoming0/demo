e73d4c4a0e110b414a8140df672cdde1bd49db7b
zhaoming0 <mingx.zhao@intel.com>
2017-04-18 18:15:23 +0800

    update

--- resources/testharness.js
+++ resources/testharness.js
@@ -547,9 +547,6 @@ policies and contribution forms [3].
             });
             var promise = test.step(func, test, test);
             test.step(function() {
-              console.log("line 550");
-              console.log(promise);
-              console.log("line 553");
                 assert_not_equals(promise, undefined);
             });
             Promise.resolve(promise).then(
@@ -569,15 +566,6 @@ policies and contribution forms [3].
     }
 
     function promise_rejects(test, expected, promise, description) {
-        console.log('DEBUG-1')
-        console.log(test)
-        console.log('DEBUG-2')
-        console.log(expected)
-        console.log('DEBUG-3')
-        console.log(promise)
-        console.log('DEBUG-4')
-        console.log(description)
-        console.log('DEBUG-5')
         return promise.then(test.unreached_func("Should have rejected: " + description)).catch(function(e) {
             assert_throws(expected, function() { throw e }, description);
         });
@@ -930,10 +918,6 @@ policies and contribution forms [3].
           * Test if two primitives are unequal or two objects
           * are different objects
           */
-      console.log("Line 930");
-      console.log(actual);
-      console.log(expected);
-      console.log("Line 933");
         assert(!same_value(actual, expected), "assert_not_equals", description,
                                               "got disallowed value ${actual}",
                                               {actual:actual});
@@ -1532,7 +1516,6 @@ policies and contribution forms [3].
         this.timeout_id = null;
         this.set_status(this.TIMEOUT, "Test timed out");
         this.phase = this.phases.HAS_RESULT;
-        console.log('8888888888888888888888888888888888')
         this.done();
     };
 
